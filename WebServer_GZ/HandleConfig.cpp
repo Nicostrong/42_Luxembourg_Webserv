e07f470ab9156b18e508e490fcdc4c3ae8156220
gzenner <gzenner@student.42luxembourg.lu>
2025-04-23 13:26:01 +0200

    Tom Marvolo Riddle = Lord Voldemort

--- WebServer_GZ/HandleConfig.cpp
+++ WebServer_GZ/HandleConfig.cpp
@@ -6,15 +6,13 @@
 /*   By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 08:31:34 by gzenner           #+#    #+#             */
-/*   Updated: 2025/04/23 13:16:45 by gzenner          ###   ########.fr       */
+/*   Updated: 2025/04/23 13:24:42 by gzenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HandleConfig.hpp"
 #include <vector>
-#include <stdio.h>
 #include <string>
-#include <bits/stdc++.h>
 
 void HandleConfig::readConfigFile(const char *filename)
 {
@@ -32,13 +30,10 @@ void HandleConfig::readConfigFile(const char *filename)
 	while(getline(file, line))
 	{
 		vector.push_back(line);
-		//std::cout << vector.back() << "\n===================\n";
 	}
 	countCurlyBrackets = 0;
-	//std::map<std::string, std::string> tmpMap;
 	for(int i = 1; i < vector.size(); i++)
 	{
-		//std::cout << vector[i] << "\n===================\n";
 		found = vector[i].find('{');
 		if (found != std::string::npos)
 		{
@@ -50,7 +45,6 @@ void HandleConfig::readConfigFile(const char *filename)
 			while(countCurlyBrackets > 0)
 			{
 				concat += vector[i];
-				//std::cout << "[debug} concat" << vector[i] << "\n===================\n";
 				found = vector[i].find("{");
 				if (found != std::string::npos)
 					countCurlyBrackets++;
@@ -65,12 +59,10 @@ void HandleConfig::readConfigFile(const char *filename)
 				i++;
 			}
 			tmpMap[key] = concat;
-			//std::cout << "" << key << " " << tmpMap[key] << "\n===================\n";
 		}
 		else
 		{
 			tmpMap[vector[i]] = vector[i];
-			//std::cout << "" << vector[i] << " " << tmpMap[vector[i]] << "\n===================\n";
 		}	
 	}
 	file.close();
@@ -78,26 +70,19 @@ void HandleConfig::readConfigFile(const char *filename)
 
 void HandleConfig::handleSimpleLine(std::string& line)
 {
-	//std::cout << "[debug simple line]." << line << "." << "\n";
 	std::string key, value;
 	key = line;
 	std::size_t start = key.find_first_not_of(" \t");
 	key.erase(0, start);
-	std::size_t end1 = key.find("\t");
-	std::size_t end2 = key.find(" ");
-	std::size_t end;
-	if (end1 == std::string::npos && end2 == std::string::npos)
+	std::size_t end = key.find_first_of(" \t");
+	if (end != std::string::npos)
 	{
-		std::cout << "Error: End of Word not found.\n";
-		return;
+		key.erase(end, key.size());
 	}
-	if (end1 != std::string::npos)
-		end = end1;
-	if (end2 != std::string::npos)
-		end = end2;
-	key.erase(end, key.size());
-	//std::cout << "." << key << "." << "\n";
-	
+	else
+	{
+		std::cout << "Error: End of Word not found.\n";	
+	}
 	// =============================================
 
 	value = line;
@@ -107,25 +92,17 @@ void HandleConfig::handleSimpleLine(std::string& line)
 	value.erase(0, start);
 	start = value.find_first_not_of(" \t");
 	value.erase(0, start);
-	//std::cout << "." << value << "." << "\n";
 	
 	this->nicoMap[key] = value;
 }
 
 void HandleConfig::handleObjLine(std::string& first, std::string& second)
 {
-	//std::cout << "[debug obj first]" << first << "\n";
-	//std::cout << "[debug obj second]" << second << "\n";
 	std::string key = first;
 	key.erase(0, key.find_first_not_of(" \t"));
 	key.erase(key.find_first_of("{"), key.size());
 	key.erase(key.find_last_not_of(" /t")+1, key.size());
-	//std::cout << "[debug key]" << "." << key << "." << "\n";
-	
-	std::string value = second;
-	//value.erase(value.find_first_of("{"), value.size());
-	//std::cout << "[debug value]" << "." << value << "." << "\n";
-	this->nicoMap[key] = value;
+	this->nicoMap[key] = second;
 }
 
 void HandleConfig::cleanMap()
