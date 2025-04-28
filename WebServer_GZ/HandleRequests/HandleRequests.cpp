eb00e9fb122871e912ae00d5a4d742ef16087320
gzenner <gzenner@student.42luxembourg.lu>
2025-04-28 13:37:04 +0200

    HandleRequests Update

--- WebServer_GZ/HandleRequests/HandleRequests.cpp
+++ WebServer_GZ/HandleRequests/HandleRequests.cpp
@@ -24,13 +24,41 @@ const std::string& HandleRequests::getMethodRules(std::string type)
 	bool DENY;
 	bool ALLOW;
 	bool ALL;
+	std::size_t found;
 
 	HandleConfig hc("webserv.conf");
 
-	std::string rules = hc.getValue(type);
-	/*std::cout << "[debug] map: " << webconfMap["error_page"] << "\n";
-	rules = webconfMap[type];*/
+	webconfMap = hc.getwebconfMap();
+	std::map<std::string, std::string>::iterator it = webconfMap.begin();
+	while(it != webconfMap.end())
+	{
+		found = it->first.find(type);
+		if (found != std::string::npos)
+		{
+			std::string rules = it->second;
+			found = rules.find("limit_except");
+			if (found != std::string::npos)
+				rules.erase(0, found);
+
+			std::cout << "[debug] rules:" << rules << "\n";
+
+			GET = rules.find("GET") != std::string::npos;
+			PUT = rules.find("PUT") != std::string::npos;
+			DELETE = rules.find("DELETE") != std::string::npos;
+			DENY = rules.find("DENY") != std::string::npos;
+			ALLOW = rules.find("ALLOW") != std::string::npos;
+			ALL = rules.find("ALL") != std::string::npos;
+			std::cout << "debugging getMethodRules map:" << it->first << "\n=======\n" << it->second << "\n--------\n";
+		}
+		//std::cout << "debugging getMethodRules map:" << it->first << "\n=======\n" << it->second << "\n--------\n";
+		++it;
+	}
+/*
+	hc.printwebconfMap();
+	std::string rules = hc.getValue("error_page");
 	std::cout << "[debug] doing getMethodRules: " << rules  << "size : " << rules.size() << "\n";
+	std::cout << "[debug] map: " << webconfMap["error_page"] << "\n";
+	rules = webconfMap[type];*/
 	/*if (rules.size() > 0)
 	{
 		std::size_t found = rules.find("limit_except");
@@ -46,6 +74,7 @@ const std::string& HandleRequests::getMethodRules(std::string type)
 		ALLOW = rules.find("ALLOW") != std::string::npos;
 		ALL = rules.find("ALL") != std::string::npos;
 	}*/
+	return "DEBUG\n";
 }
 
 void HandleRequests::ExecuteRequest()
