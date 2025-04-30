6d4b98d23cc57ac7cb5bb5f1c79e5c34a41fb7b5
gzenner <gzenner@student.42luxembourg.lu>
2025-04-30 16:08:44 +0200

    HandleCGI First 2 HTMLs

---
+++ GZ/HandleCGI/update_register_newsletter.py
@@ -0,0 +1,12 @@
+with open("register_newsletter_base.html", "r") as file:
+	content = file.read()
+
+username = "Flavian"
+useremail = "Flavian@email.com"
+
+content = content.replace("GREETING", "Hello World, CGI is working!")
+content = content.replace("USERNAME", username)
+content = content.replace("USEREMAIL", useremail)
+
+with open("register_newsletter.html", "w+") as file:
+	file.write(content)
---
+++ GZ/HandleCGI/update_showdata.py
@@ -0,0 +1,18 @@
+import sys
+
+if len(sys.argv) != 5:
+	exit()
+
+with open("showdata_base.html", "r") as file:
+	content = file.read()
+
+username = "Flavian"
+useremail = "Flavian@email.com"
+
+content = content.replace("DATA1", sys.argv[1])
+content = content.replace("DATA2", sys.argv[2])
+content = content.replace("DATA3", sys.argv[3])
+content = content.replace("DYNSTR", sys.argv[4])
+
+with open("showdata_userid.html", "w+") as file:
+	file.write(content)
