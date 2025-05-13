302bba537ad408626de2cf59e0630868cdec8972
gzenner <gzenner@student.42luxembourg.lu>
2025-05-13 08:59:14 +0200

    Upload and GetEnv

--- GZ/HandleCGI/UploadFile.py
+++ GZ/HandleCGI/UploadFile.py
@@ -1,9 +1,12 @@
+import sys
 import streamlit as st
 
 st.write("File Uploader")
 
 content = st.file_uploader("Upload file")
 
-if content is not None and not os.path.isfile("userinput.txt"):
-    with open("userinput.txt", "w+") as file:
-        file.write(content)
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    if content is not None and not os.path.isfile(filename):
+        with open(filename, "w+") as file:
+            file.write(content)
