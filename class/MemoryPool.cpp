8d4781d41696ae8afbf364512d45b75b7b34fe97
cinqdtun <contact@5dt1.com>
2025-05-22 23:34:14 +0200

    Implemented shared memory pool

--- class/MemoryPool.cpp
+++ class/MemoryPool.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MemoryPool.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdehan <fdehan@student.42luxembourg.lu>    +#+  +:+       +#+        */
+/*   By: fdehan <fdehan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 18:18:30 by fdehan            #+#    #+#             */
-/*   Updated: 2025/05/22 18:54:54 by fdehan           ###   ########.fr       */
+/*   Updated: 2025/05/22 23:33:46 by fdehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 MemoryPool::MemoryPool() 
 {
 	this->_smallBuffers.reserve(MAX_CLIENT);
-	this->_bigBuffers.reserve(DHEAD_BIG_BUFF_N);;
+	this->_mediumBuffers.reserve(MEDIUM_AMOUNT);
+	this->_largeBuffers.reserve(MAX_CLIENT);;
 }
 
 MemoryPool::MemoryPool(const MemoryPool& obj) : 
-	_smallBuffers(obj._smallBuffers), _bigBuffers(obj._bigBuffers), _smallAvailables(obj._smallAvailables), _bigAvailables(obj._bigAvailables) {}
+	_smallBuffers(obj._smallBuffers), _mediumBuffers(obj._mediumBuffers), 
+	_largeBuffers(obj._largeBuffers), _smallAvailables(obj._smallAvailables), 
+	_mediumAvailables(obj._mediumAvailables), _largeAvailables(obj._largeAvailables) {}
 
 MemoryPool::~MemoryPool() {}
 
@@ -27,10 +30,80 @@ MemoryPool MemoryPool::operator=(const MemoryPool& obj)
 {
 	if (this != &obj)
 	{
-		this->_bigBuffers = obj._bigBuffers;
 		this->_smallBuffers = obj._smallBuffers;
+		this->_smallAvailables = obj._smallAvailables;
+		this->_mediumBuffers = obj._mediumBuffers;
+		this->_mediumAvailables = obj._mediumAvailables;
+		this->_largeBuffers = obj._largeBuffers;
+		this->_largeAvailables = obj._largeAvailables;
 	}
 	return (*this);
 }
 
-void 
+Buffer* MemoryPool::getSmallBufferFromPool()
+{
+	return (getBufferFromPool(this->_largeBuffers, this->_largeAvailables, 
+		MEDIUM_BUFF));
+}
+
+Buffer* MemoryPool::getMediumBufferFromPool()
+{
+	return (getBufferFromPool(this->_mediumBuffers, this->_mediumAvailables, 
+		MEDIUM_BUFF));
+}
+
+Buffer*	MemoryPool::getLargeBufferFromPool()
+{
+	return (getBufferFromPool(this->_largeBuffers, this->_largeAvailables, 
+		LARGE_BUFF));
+}
+
+Buffer* MemoryPool::getBufferFromPool(std::vector<Buffer*>& buffs, std::queue<Buffer*>& available, size_t buffSize)
+{
+	Buffer* buff;
+	if (available.size() < 1)
+	{
+		if (buffs.size() >= buffs.capacity())
+			throw NoAvailableBuffer();
+		buff = new Buffer(buffSize);
+		if (!buff)
+			throw NoAvailableBuffer();
+		buffs.push_back(buff);
+		return (buff);
+	}
+	buff = available.front();
+	available.pop();
+	return (buff);
+}
+
+void MemoryPool::setAvailableBuffer(Buffer* buff)
+{
+	size_t buffSize = buff->getVector().size();
+
+	switch (buffSize)
+	{
+		case SMALL_BUFF:
+			this->_smallAvailables.push(buff);
+			break;
+		case MEDIUM_BUFF:
+			this->_mediumAvailables.push(buff);
+			break;
+		case LARGE_BUFF:
+			this->_largeAvailables.push(buff);
+			break;
+		default:
+			throw InvalidBuffer();
+	}
+}
+
+// Exceptions
+
+const char *MemoryPool::NoAvailableBuffer::what() const throw()
+{
+	return ("No available buffer");
+}
+
+const char *MemoryPool::InvalidBuffer::what() const throw()
+{
+	return ("Invalid buffer");
+}
--- includes/MemoryPool.hpp
+++ includes/MemoryPool.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MemoryPool.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdehan <fdehan@student.42luxembourg.lu>    +#+  +:+       +#+        */
+/*   By: fdehan <fdehan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 18:13:04 by fdehan            #+#    #+#             */
-/*   Updated: 2025/05/22 18:47:47 by fdehan           ###   ########.fr       */
+/*   Updated: 2025/05/22 23:33:12 by fdehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,43 @@
 #include "lib.hpp"
 #include "Buffer.hpp"
 
-# define DHEAD_BUFF 1024
-# define DBIG_HEAD_BUFF 8192
-# define DHEAD_BIG_BUFF_N 4
+# define SMALL_BUFF 1024
+# define MEDIUM_BUFF 8192
+# define MEDIUM_AMOUNT 4
+# define LARGE_BUFF 16384
+
+
 # define MAX_CLIENT 1024
 
 class MemoryPool
 {
 	public:
-		enum Type
-		{
-			SMALL = 0,
-			BIG = 1,	
+		class NoAvailableBuffer : public std::exception {
+			public:
+				const char * what () const throw();
+		};
+		class InvalidBuffer : public std::exception {
+			public:
+				const char * what () const throw();
 		};
 		MemoryPool();
 		MemoryPool(const MemoryPool& obj);
 		~MemoryPool();
-		MemoryPool operator=(const MemoryPool& obj);
+		MemoryPool	operator=(const MemoryPool& obj);
+		Buffer*		getSmallBufferFromPool();
+		Buffer*		getMediumBufferFromPool();
+		Buffer*		getLargeBufferFromPool();
+		void 		setAvailableBuffer(Buffer* buff);
 	private:
+		Buffer*		getBufferFromPool(std::vector<Buffer*>& buffs, 
+			std::queue<Buffer*>& available, size_t buffSize);
+		
 		std::vector<Buffer*>	_smallBuffers;
-		std::vector<Buffer*> 	_bigBuffers;
+		std::vector<Buffer*> 	_mediumBuffers;
+		std::vector<Buffer*>	_largeBuffers;
 		std::queue<Buffer*>		_smallAvailables;
-		std::queue<Buffer*>		_bigAvailables;
-		size_t					_small;
+		std::queue<Buffer*>		_mediumAvailables;
+		std::queue<Buffer*>		_largeAvailables;
 };
 
 # endif
