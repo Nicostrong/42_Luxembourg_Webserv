57ecd7f1a96ced8d81fbf58b3791aecd59b5d6a7
cinqdtun <contact@5dt1.com>
2025-06-05 10:05:12 +0200

    Created cgi parser object

---
+++ class/parser/CgiParser.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiParser.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdehan <fdehan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/05 10:01:42 by fdehan            #+#    #+#             */
+/*   Updated: 2025/06/05 10:04:48 by fdehan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../../includes/parser/CgiParser.hpp"
+
+CgiParser::CgiParser() 
+{
+    this->_headBuffer.reserve(CGI_HEAD_BUFF);
+}
+
+CgiParser::~CgiParser() {}
