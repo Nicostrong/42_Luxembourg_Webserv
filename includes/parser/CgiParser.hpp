2d1ace33ea79bef232e712facce394620985ed38
cinqdtun <contact@5dt1.com>
2025-06-05 11:30:19 +0200

    Update todo

--- includes/parser/CgiParser.hpp
+++ includes/parser/CgiParser.hpp
@@ -6,7 +6,7 @@
 /*   By: fdehan <fdehan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 10:00:57 by fdehan            #+#    #+#             */
-/*   Updated: 2025/06/05 10:04:31 by fdehan           ###   ########.fr       */
+/*   Updated: 2025/06/05 10:41:08 by fdehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 # define CGIPARSER_HPP
 
 #include "./../lib.hpp"
+#include "./../utils/Buffer.hpp"
 
 #define CGI_HEAD_BUFF 8192
 
 class CgiParser
 {
     public:
+        enum State
+        {
+			HEAD_RECEVING = 0,
+			BODY_RECEIVING = 1,
+			ERROR = 2,
+        };
         CgiParser();
         ~CgiParser();
+		void	onData(Buffer &buff);
     private:
         CgiParser(const CgiParser& obj);
         CgiParser& operator=(const CgiParser& obj);
