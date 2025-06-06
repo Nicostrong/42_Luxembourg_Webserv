3e8391edc0570d0cc9658819aab3ad8ec5688b18
cinqdtun <contact@5dt1.com>
2025-06-06 10:06:04 +0200

    Creating helpers functions for cgi handling to check headers values

---
+++ includes/handling/CgiHandling.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiHandling.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdehan <fdehan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 09:12:54 by fdehan            #+#    #+#             */
+/*   Updated: 2025/06/06 10:02:22 by fdehan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CGIHANDLING_HPP
+# define CGIHANDLING_HPP
+
+#include "./lib.hpp"
+
+class CgiHandling
+{
+    public:
+    private:
+        CgiHandling();
+        CgiHandling(const CgiHandling& obj);
+        ~CgiHandling();
+        CgiHandling& operator=(const CgiHandling& obj);
+
+        static bool isStatusHeaderValid(const std::string& status);
+        static bool isLocationValid(const std::string& loc);
+};
+
+#endif
