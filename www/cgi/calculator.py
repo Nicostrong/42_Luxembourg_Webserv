17f7774b226cbd279445e7bb2781f203f045c23e
cinqdtun <contact@5dt1.com>
2025-07-02 12:52:11 +0200

    Calculator cgi fix

--- www/cgi/calculator.py
+++ www/cgi/calculator.py
@@ -1,16 +1,17 @@
 import sys
 import os
+import json
 
 print("Status: 200", end='\r\n')
 print("Content-Type: text/html; charset=utf-8\r\n", end='\r\n')
 print()
 
 content_length = int(os.environ.get('CONTENT_LENGTH', 0))
-line = sys.stdin.read(content_length).split(' ')
-
-num1 = float(line[0])
-operator = line[1]
-num2 = float(line[2])
+line = sys.stdin.read(content_length)
+data = json.loads(line)
+num1 = float(data.get('num1', 0))
+num2 = float(data.get('num2', 0))
+operator = data.get('operator', '')
 
 if operator == '+':
     result = num1 + num2
