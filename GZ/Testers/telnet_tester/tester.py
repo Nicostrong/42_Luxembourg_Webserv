336fc2984aa072407c866bc2271702bfd3b66dc0
gzenner <gzenner@student.42luxembourg.lu>
2025-07-03 14:32:29 +0200

    telnet

--- GZ/Testers/telnet_tester/tester.py
+++ GZ/Testers/telnet_tester/tester.py
@@ -7,7 +7,7 @@ PORT = 2121
 if len(sys.argv) < 2:
     request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
 else:
-    request = "GET /" + sys.argv[1] + " HTTP/1.1\r\nHost: localhost\r\n\r\n"
+    request = f"GET {sys.argv[1]} HTTP/1.1\r\nHost: {sys.argv[3]}\r\n\r\n"
     PORT = int(sys.argv[2])
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
