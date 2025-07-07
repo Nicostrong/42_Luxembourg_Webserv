daa29ee6970f268a5d199bd03a75102870c615e9
gzenner <gzenner@student.42luxembourg.lu>
2025-07-07 12:55:21 +0200

    Cheer Up

---
+++ www/cgi/cheerup.py
@@ -0,0 +1,30 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    cheerup.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/07/02 14:44:21 by nfordoxc          #+#    #+#              #
+#    Updated: 2025/07/07 12:54:39 by gzenner          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+import os
+import json
+import string
+from string import ascii_lowercase, ascii_uppercase
+import random
+
+print("Status: 200", end='\r\n')
+print("Content-Type: text/html; charset=utf-8\r\n", end='\r\n')
+print()
+
+content_length = int(os.environ.get('CONTENT_LENGTH', 0))
+line = sys.stdin.read(content_length)
+data = json.loads(line)
+problem = (data.get('problem', ''))
+#problem = "Pointers"
+
+print(f"Cheer up!\n{problem} really sucks! Everybody hates it!")
