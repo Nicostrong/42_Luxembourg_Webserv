1b69a76065d23e7d287497207ef6652eacb82309
gzenner <gzenner@student.42luxembourg.lu>
2025-07-07 11:29:19 +0200

    Caesar

---
+++ www/cgi/caesar.py
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    caesar.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/07/02 14:44:21 by nfordoxc          #+#    #+#              #
+#    Updated: 2025/07/07 11:28:35 by gzenner          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+import os
+import json
+import random
+
+print("Status: 200", end='\r\n')
+print("Content-Type: text/html; charset=utf-8\r\n", end='\r\n')
+print()
+
+content_length = int(os.environ.get('CONTENT_LENGTH', 0))
+line = sys.stdin.read(content_length)
+data = json.loads(line)
+text = (data.get('text', ''))
+shift = int(data.get('shift', 0))
+shift %= 126
+
+encrypted_text = ""
+for c in text:
+    newvalue = ord(c) + shift
+    newvalue %= (126-32)
+    newvalue += 32
+    encrypted_text += chr(newvalue)
+
+print(f"Original Text{text}")
+print(f"Encryped Text{encrypted_text}")
