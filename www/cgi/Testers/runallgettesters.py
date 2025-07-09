9981de5cf8fa4ddb302bed203547797dfafc0d11
gzenner <gzenner@student.42luxembourg.lu>
2025-07-09 09:14:13 +0200

    Tester Update

--- www/cgi/Testers/runallgettesters.py
+++ www/cgi/Testers/runallgettesters.py
@@ -6,7 +6,7 @@
 #    By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/08 10:59:30 by gzenner           #+#    #+#              #
-#    Updated: 2025/07/08 11:12:26 by gzenner          ###   ########.fr        #
+#    Updated: 2025/07/09 09:13:27 by gzenner          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,10 @@ localhost = "localhost"
 shift = "50"
 
 sp.run(["clear"])
+print("Good Requests:")
 sp.run(["python3", "get_tester.py", port, localhost, "/"])
+
+print("Bad Requests:")
 sp.run(["python3", "get_tester.py", port, localhost, "/a"])
 sp.run(["python3", "get_tester.py", port, localhost, "/asasccasacs"])
 sp.run(["python3", "get_tester.py", port, localhost, "/index.html"])
